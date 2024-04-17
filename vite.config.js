0305adec5c9fa30fbe9e98632677926bd7ba4234
Samuel Navarro <ti43519@uvp.edu.mx>
2024-04-16 20:33:35 -0600

    base url try 3

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
-  //,
-  //base:'https://danncruise.github.io/juegos-vue/'
+  plugins: [vue()],
+  base:'https://ti43519.github.io/juego-vue-t3/'
 })
